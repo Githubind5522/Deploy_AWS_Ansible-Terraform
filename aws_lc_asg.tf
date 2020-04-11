808dcbdda14724d487e2dbef8c1a2d5acd523e1a
Naresh240 <58024415+Naresh240@users.noreply.github.com>
2020-04-11 22:07:55 +0530

    Update aws_lc_asg.tf

--- aws_lc_asg.tf
+++ aws_lc_asg.tf
@@ -9,7 +9,6 @@ resource "aws_launch_configuration" "packer_lc" {
 resource "aws_autoscaling_group" "packer_asg" {
   name                 = "terraform-asg-${aws_launch_configuration.packer_lc.name}"
   launch_configuration = "${aws_launch_configuration.packer_lc.name}"
-  availability_zones = ["${data.aws_availability_zones.allzones.names}"]
   min_size             = 2
   max_size             = 3
   load_balancers = ["${aws_elb.terra-elb.id}"]
